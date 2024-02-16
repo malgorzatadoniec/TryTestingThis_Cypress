d61409fd1b677fb87920cb1224d70502fa2afb05
malgorzatadoniec <malgorzata.doniec96@gmail.com>
2024-02-16 12:20:02 +0100

    scenario2

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://trytestingthis.netlify.app/',
+    watchForFileChanges: false
   },
 });
